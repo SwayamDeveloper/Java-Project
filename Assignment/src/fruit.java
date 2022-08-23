be7415d09fb47b41cd58101df9e80558d65f2ebc
swaya <swaya@DESKTOP-H27PH04>
2022-08-23 23:14:59 +0530

    push and commit

---
+++ Assignment/src/fruit.java
@@ -0,0 +1,14 @@
+
+
+public class fruit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String fruits[]=new String[] {"apple","banana","mango","cherry"};
+		for(String x:fruits)
+		{
+			System.out.println(x);
+		}
+	}
+
+}
